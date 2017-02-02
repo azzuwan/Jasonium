d5cbe122dcda7f7716d52e7aae06156369a4450f
Azzuwan Aziz <azzuwan@gmail.com>
2017-02-03 03:22:29 +0800

    Basic context menu for the server tree

---
+++ src/main/java/com/blazzify/jasonium/ui/ServerTreeContextMenu.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blazzify.jasonium.ui;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+
+/**
+ *
+ * @author Azzuwan Aziz <azzuwan@gmail.com>
+ */
+public class ServerTreeContextMenu extends ContextMenu {
+
+    MenuItem connect = new MenuItem("Connect");
+    MenuItem disconnect = new MenuItem("Disconnect");
+    MenuItem remove = new MenuItem("Remove");
+
+    public ServerTreeContextMenu() {
+        connect.setOnAction((event) -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Connecting!", ButtonType.OK);
+        });
+        disconnect.setOnAction((event) -> {
+        });
+        remove.setOnAction((event) -> {
+        });
+        this.getItems().addAll(connect, disconnect, remove);
+    }
+
+}
